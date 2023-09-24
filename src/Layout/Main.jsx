c11932974e97d28040e3c6cc662306a4eff86e5a
Astro <rgrxmahabub.official@gmail.com>
2023-09-24 15:44:21 +0600

    initiated

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "../shared/Navbar/NavBar";
+import Footer from "../shared/Footer/Footer";
+
+const Main = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Main;

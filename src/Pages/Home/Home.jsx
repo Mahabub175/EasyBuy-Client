faef290f14cfe0b7f9361143af9b40c0ad96e24a
Astro <rgrxmahabub.official@gmail.com>
2023-10-05 21:22:52 +0600

    [Done] Banner

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,19 +1,19 @@
+import Banner from "./Banner/Banner";
 import ExtraService from "./ExtraService/ExtraService";
 import RecommandedItems from "./RecommandedItems/RecommandedItems";
 import SubscribeField from "./SubscribeField/SubscribeField";
 import SuppliersRegion from "./SuppliersRegion/SuppliersRegion";
 
 const Home = () => {
-  return <div>
-
-    This is home
-    <RecommandedItems></RecommandedItems>
-    <ExtraService></ExtraService>
-    <SuppliersRegion></SuppliersRegion>
-    <SubscribeField></SubscribeField>
-
-    
-  </div>;
+  return (
+    <div>
+      <Banner />
+      <RecommandedItems></RecommandedItems>
+      <ExtraService></ExtraService>
+      <SuppliersRegion></SuppliersRegion>
+      <SubscribeField></SubscribeField>
+    </div>
+  );
 };
 
 export default Home;
